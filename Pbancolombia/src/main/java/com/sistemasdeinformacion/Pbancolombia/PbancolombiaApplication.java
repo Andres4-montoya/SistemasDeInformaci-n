a82080eaaa14e9ec4dbf62f1b3657f12a54d1cf6
Andres Felipe Montoya Bedoya <66339464+Andres4-montoya@users.noreply.github.com>
2025-09-01 18:33:51 -0500

    Add files via upload

---
+++ Pbancolombia/src/main/java/com/sistemasdeinformacion/Pbancolombia/PbancolombiaApplication.java
@@ -0,0 +1,13 @@
+package com.sistemasdeinformacion.Pbancolombia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PbancolombiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PbancolombiaApplication.class, args);
+	}
+
+}
---
+++ Pbancolombia/src/test/java/com/sistemasdeinformacion/Pbancolombia/PbancolombiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemasdeinformacion.Pbancolombia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PbancolombiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
